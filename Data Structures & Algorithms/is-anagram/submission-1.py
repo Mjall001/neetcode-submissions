0347895520dc6afab13716dd42ba1d950d0b315f
Mjall001 <148555115+Mjall001@users.noreply.github.com>
2026-09-11 12:05:57 +0100

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #Check if the character is alphanum
+        #Cycle through one set comparing the number of times each character appears
+
+        dictS, dictT = {}, {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            dictS[s[i]] = 1 + dictS.get(s[i], 0)
+            dictT[t[i]] = 1 + dictT.get(t[i], 0)
+
+        return dictT == dictS
+        
+
+     
