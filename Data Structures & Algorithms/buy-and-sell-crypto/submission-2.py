98d53f52c86200d67e5ed999cb05787ac654dadd
Mjall001 <148555115+Mjall001@users.noreply.github.com>
2026-09-19 08:41:32 +0100

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # sliding window problem
+        # differences between smallest and largest value
+        # only shift left when it's bigger than right
+        # right moves regardless
+        # save biggest profit in maxProfit variable
+        # compare to currentProfit
+
+        l, r = 0, 1
+        maxProfit, currentProfit = 0, 0
+        while r in range(len(prices)):
+            currentProfit = prices[r] - prices[l]
+            maxProfit = max(maxProfit, currentProfit)
+            if prices[r] < prices[l]:
+                l=r
+            r=r+1
+            
+        return maxProfit
+            
+
+
+
